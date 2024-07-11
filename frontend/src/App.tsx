dba50378a53b0377b981ba0630741d720bf60564
lz-junior <correajr.dev@gmail.com>
2024-07-10 22:50:26 -0300

    styled inputs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { MapPin, Calendar, ArrowRight } from 'lucide-react';
 
 export function App() {
   return (
@@ -6,10 +6,23 @@ export function App() {
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg">Invite your friends and plan your next trip!</p>
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center">
-          <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400" />
-          <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400" />
-          <button>Continue</button>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
+          </div>
+
+          <div className="flex items-center gap-2 ">
+            <Calendar className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800"></div>
+
+          <button className="h-10 bg-lime-300 text-lime-950 rounded-lg px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continue
+            <ArrowRight className="size-5"/>
+          </button>
         </div>
 
         <p className="text-sm text-zinc-500">
