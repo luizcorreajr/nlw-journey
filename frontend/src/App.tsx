6f06042baab454c0460b37227202995e93fab8cf
lz-junior <correajr.dev@gmail.com>
2024-07-10 23:10:12 -0300

    styled inputs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
           <div className="flex items-center gap-2 flex-1">
             <MapPin className="size-5 text-zinc-400"/>
-            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
+            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
           </div>
 
           <div className="flex items-center gap-2 ">
