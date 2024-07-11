a71f2ed48cb672d9d8a29c064f34bab49d1f2740
lz-junior <correajr.dev@gmail.com>
2024-07-10 21:55:02 -0300

    styled with tailwind in app.tsx - lucide react installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,21 @@
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">Invite your friends and plan your next trip!</p>
+        
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center">
+          <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400" />
+          <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400" />
+          <button>Continue</button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          By planning your trip with plann.er, you automatically <br/> 
+          agree to our <a className="text-zinc-300 underline" href="#">terms of use</a> and <a className="text-zinc-300 underline" href="#">privacy policies</a>.
+        </p>
+      </div>
+    </div>
   )
 }
