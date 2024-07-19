ed9402b38990538d9541a9627e0ff898eeb0ce7c
lz-junior <correajr.dev@gmail.com>
2024-07-19 00:38:48 -0300

    add Destination and date step OK

--- frontend/src/pages/create-trip/index.tsx
+++ frontend/src/pages/create-trip/index.tsx
@@ -1,8 +1,9 @@
 import { FormEvent, useState } from 'react';
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react';
+import { ArrowRight, UserRoundPlus } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { InviteGuestsModal } from './invite-guests-modal';
 import { ConfirmTripModal } from './confirm-trip-modal';
+import { DestinationAndDateStep } from './steps/destination-and-date-step';
 
 export function CreateTripPage() {
   const navigate = useNavigate()
@@ -85,32 +86,12 @@ export function CreateTripPage() {
         </div>
         
         <div className="space-y-4">
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
-            <div className="flex items-center gap-2 flex-1">
-              <MapPin className="size-5 text-zinc-400"/>
-              <input disabled={isGuestsInputOpen} type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-            </div>
-
-            <div className="flex items-center gap-2 ">
-              <Calendar className="size-5 text-zinc-400"/>
-              <input disabled={isGuestsInputOpen} type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            </div>
-
-            <div className="w-px h-6 bg-zinc-800"/>
-
-            {isGuestsInputOpen ? (
-              <button onClick={closeGuestsInput} className="h-10 bg-zinc-800 text-zinc-200 rounded-lg px-5 font-medium flex items-center gap-2 hover:bg-zinc-700">
-                Change location/date
-                <Settings2 className="size-5"/>
-              </button>
-            ) : (
-              <button onClick={openGuestsInput} className="h-10 bg-lime-300 text-lime-950 rounded-lg px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
-                Continue
-                <ArrowRight className="size-5"/>
-            </button>
-            )}
-          </div>
-
+          <DestinationAndDateStep 
+            isGuestsInputOpen={isGuestsInputOpen}
+            closeGuestsInput={closeGuestsInput}
+            openGuestsInput={openGuestsInput}
+          />
+           
           {isGuestsInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
               <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
---
+++ frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,41 @@
+import { MapPin, Calendar, Settings2, ArrowRight } from "lucide-react"
+
+interface DestinationAndDateStepProps {
+  isGuestsInputOpen: boolean
+  closeGuestsInput: () => void
+  openGuestsInput: () => void
+}
+
+export function DestinationAndDateStep({
+  isGuestsInputOpen,
+  closeGuestsInput,
+  openGuestsInput,
+}: DestinationAndDateStepProps) {
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+      <div className="flex items-center gap-2 flex-1">
+        <MapPin className="size-5 text-zinc-400"/>
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      </div>
+
+      <div className="flex items-center gap-2 ">
+        <Calendar className="size-5 text-zinc-400"/>
+        <input disabled={isGuestsInputOpen} type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+      </div>
+
+      <div className="w-px h-6 bg-zinc-800"/>
+
+      {isGuestsInputOpen ? (
+        <button onClick={closeGuestsInput} className="h-10 bg-zinc-800 text-zinc-200 rounded-lg px-5 font-medium flex items-center gap-2 hover:bg-zinc-700">
+          Change location/date
+          <Settings2 className="size-5"/>
+        </button>
+      ) : (
+        <button onClick={openGuestsInput} className="h-10 bg-lime-300 text-lime-950 rounded-lg px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+          Continue
+          <ArrowRight className="size-5"/>
+        </button>
+      )}
+    </div>
+  )
+}
