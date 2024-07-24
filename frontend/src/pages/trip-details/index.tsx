e193201933ea712e1c862c014a59964861cc414f
lz-junior <correajr.dev@gmail.com>
2024-07-24 00:34:22 -0300

    page trip details OK

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -70,7 +70,6 @@ export function TripDetailsPage() {
         </div>
         
         <div className="w-80 space-y-6">
-
           <div className="space-y-6">
             <h2 className=" font-semibold text-xl">Important Links</h2>
             <div className="space-y-5">
@@ -98,7 +97,29 @@ export function TripDetailsPage() {
           <div className="w-full h-px bg-zinc-800"/>
 
           <div>
-
+          <div className="space-y-6">
+            <h2 className=" font-semibold text-xl">Guests</h2>
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Bruna Carvalho</span>
+                  <span className="block text-sm text-zinc-400 truncate">brunaca@gmail.com</span>
+                </div>
+                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+              </div>
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Pedro Coimbra</span>
+                  <span className="block text-sm text-zinc-400 truncate">pedraocoimbra94@outlook.com</span>
+                </div>
+                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+              </div>
+            </div>
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <UserCog className="size-5 "/>
+              Manage guests
+            </button>
+          </div>
           </div>
 
         </div>
