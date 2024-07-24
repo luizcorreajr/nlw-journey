371279145f103c37caa7705c5c0912a02b8ad4cc
lz-junior <correajr.dev@gmail.com>
2024-07-24 00:19:15 -0300

    part 2a - important links

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -69,7 +69,37 @@ export function TripDetailsPage() {
           </div>
         </div>
         
-        <div className="w-80">
+        <div className="w-80 space-y-6">
+
+          <div className="space-y-6">
+            <h2 className=" font-semibold text-xl">Important Links</h2>
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">AirBnB Reservation</span>
+                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011</a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+              </div>
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">House rules</span>
+                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.notion.com/pages/1047000112354648336?adults=13&children=0&infants=0&pets=0&wishlist_item_id=11003621872995&check_in=2024-08-17&check_out=2024-08-26&source_impression_id=p3_1717600906_P3DL0E-bJZzguEci&previous_page_section_name=1000</a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+              </div>
+            </div>
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <Plus className="size-5 "/>
+              Register new link
+            </button>
+          </div>
+
+          <div className="w-full h-px bg-zinc-800"/>
+
+          <div>
+
+          </div>
 
         </div>
       </main>
