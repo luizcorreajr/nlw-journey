411b3ff6bb3ef4f99d1db71f87508372041ab9ad
lz-junior <correajr.dev@gmail.com>
2024-07-25 23:23:58 -0300

    modal started

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 
+
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true)
+  }
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
     <div className="max-w-6xl px-4 py-10 mx-auto space-y-8">
-      {/* header */}
-      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+      <header className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
         
         <div className="flex items-center gap-2">
           <MapPin className="size-5 text-zinc-400"/>
@@ -25,7 +35,7 @@ export function TripDetailsPage() {
           </button>
         </div>
 
-      </div>
+      </header>
 
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
@@ -124,6 +134,48 @@ export function TripDetailsPage() {
 
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Confirm travel creation</h2>
+                <button type="button" onClick={closeConfirmTripModal}>
+                  <X className="size-5 text-zinc-400"/>
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400 ">
+                To complete the creation of your trip to <span className="font-semibold text-zinc-100">Curitiba, Brazil</span> for the dates <span className="font-semibold text-zinc-100">August 16 to 28, 2024</span>, please fill in your details below:
+              </p>
+            </div>
+
+            <form onSubmit={createTrip} className="space-y-3">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <User className="text-zinc-400"/>
+                <input 
+                    name="name"
+                    placeholder="Your full name" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <User className="text-zinc-400"/>
+                <input 
+                  type="email"
+                  name="email"
+                  placeholder="Your personal email" 
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <button type="submit" className="w-full h-11 justify-center bg-lime-300 text-lime-950 rounded-lg font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirm trip creation
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+      
     </div>
   )
 }
