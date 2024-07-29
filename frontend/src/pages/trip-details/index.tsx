186eaf1810d3bd6ceb68f0db958d815c12b6e3b0
lz-junior <correajr.dev@gmail.com>
2024-07-28 22:48:10 -0300

    created component - CreateActivityModal

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog, X, User, Tag } from "lucide-react";
-
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { CreateActivityModal } from './create-activity-modal';
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -136,48 +136,9 @@ export function TripDetailsPage() {
       </main>
 
       {isCreateActivityModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">Register activity</h2>
-                <button type="button" onClick={closeCreateActivityModal}>
-                  <X className="size-5 text-zinc-400"/>
-                </button>
-              </div>
-              <p className="text-sm text-zinc-400 ">
-                All guests can visit the activities
-              </p>
-            </div>
-
-            <form className="space-y-3">
-              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <Tag className="text-zinc-400"/>
-                <input 
-                  name="title"
-                  placeholder="What's the activity?" 
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-
-              <div className="flex items-center gap-2">
-                <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                  <Calendar className="text-zinc-400"/>
-                  <input 
-                    type="datetime-local"
-                    name="occurs_at"
-                    placeholder="Date and time" 
-                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                  />
-                </div>
-              </div>
-
-              <button type="submit" className="w-full h-11 justify-center bg-lime-300 text-lime-950 rounded-lg font-medium flex items-center gap-2 hover:bg-lime-400">
-                Save activity
-              </button>
-            </form>
-          </div>
-        </div>
+        <CreateActivityModal 
+          closeCreateActivityModal={closeCreateActivityModal}
+        />
       )}
 
     </div>
