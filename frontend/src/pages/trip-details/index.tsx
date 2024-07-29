4f5838a76f1ac7c5ecacfa34594228c1251c412c
lz-junior <correajr.dev@gmail.com>
2024-07-28 23:38:13 -0300

    create Header component

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Plus } from "lucide-react";
 import { CreateActivityModal } from './create-activity-modal';
 import { ImportantLinks } from './important-links';
 import { Guests } from './guests';
 import { Activities } from './activities';
+import { DestinationAndDateHeader } from './destination-and-date-header';
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -17,28 +18,7 @@ export function TripDetailsPage() {
 
   return (
     <div className="max-w-6xl px-4 py-10 mx-auto space-y-8">
-      <header className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
-        
-        <div className="flex items-center gap-2">
-          <MapPin className="size-5 text-zinc-400"/>
-          <span className="text-zinc-100">Curitiba, Brazil</span>
-        </div>
-        
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400"/>
-            <span className="text-zinc-100">17 a 23 de Março</span>
-          </div>
-
-          <div className="w-px h-6 bg-zinc-800"/>
-
-          <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-            Change location/date
-            <Settings2 className="size-5 "/>
-          </button>
-        </div>
-
-      </header>
+      <DestinationAndDateHeader/>
 
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
