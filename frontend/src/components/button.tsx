f43f5adf28e9ce7dfb0b6b9c2624d3b6c6da6ca6
lz-junior <correajr.dev@gmail.com>
2024-07-30 00:06:58 -0300

    tailwind variants - tv - tv variants

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,4 +1,16 @@
 import { ComponentProps, ReactNode } from "react"
+import { tv } from 'tailwind-variants'
+
+const buttonVariants = tv({
+  base: 'rounded-lg px-5 py-2 font-medium flex items-center gap-2',
+
+  variants: {
+    variant: {
+      primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
+      secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+    }
+  },
+})
 
 interface ButtonProps extends ComponentProps<'button'> {
   children: ReactNode
@@ -6,7 +18,7 @@ interface ButtonProps extends ComponentProps<'button'> {
 
 export function Button({ children, ...props }:ButtonProps) {
   return (
-    <button {...props} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+    <button {...props} className={buttonVariants({ variant: 'secondary' })}>
       {children}
     </button>
   )
